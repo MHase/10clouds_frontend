ef28ef6156686a3aaaa8626bae39e8a6730cad8d
Maciej <maciek.sykula@gmail.com>
2017-05-16 16:16:09 +0200

    html5 validation + submit form using jquery

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,29 @@
 // require('../css/style.scss');
 // import style from '../css/main.scss';
 
-let variable = 'Hello World!';
-
-console.log(variable);
 
 $(document).ready(() => {
-  // $('.content').hide();
-    // $('.content').show("slide", {direction: "left"}, 1000);
 
-    $('.radio').click(function(){
-      $('.radio, .radio *').removeClass('selected');
-      $('.radio, .radio *').find('.radio_active').css({'visibility': 'hidden'});
+  //select default 'radio button'
+  $('#female.radio, #female.radio *, #male.radio, #male.radio *').addClass('default');
+  $('#male.radio, #male.radio *').addClass('selected').find('.radio_active').css({'visibility': 'visible'});
+  //
+  //
+  //
+  //select radio, deactivate another
+  $('.radio').click(function(){
+    $('.radio, .radio *').removeClass('selected');
+    $('.radio, .radio *').find('.radio_active').css({'visibility': 'hidden'});
 
-      // $(this).addClass('active');
-      $(this).addClass('selected');
-      $(this).find('div').addClass('selected');
-      $(this).find('.radio_active').css({'visibility': 'visible'});
-      var val = $(this).attr('data-value');
-      // alert(val);
-      // $(this).parent().find('input').val(val);
-    });
+    $(this).addClass('selected');
+    $(this).find('div').addClass('selected');
+    $(this).find('.radio_active').css({'visibility': 'visible'});
+    var val = $(this).attr('data-value');
+  });
+
+  $('.submit').click(() => {
+    // $(form).submit();
+    $('#test').click();
+    // console.log('submitted');
+  });
 });
