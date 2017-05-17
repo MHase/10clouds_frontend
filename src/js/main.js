4417859c3a2eb4ab4d8f6b3e1484050cc002d57c
Maciej <maciek.sykula@gmail.com>
2017-05-17 12:08:47 +0200

    month input changed to select, started hanheld css

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,29 @@
 $(document).ready(() => {
 
+
 $('.left, .right, .tabs, .button, .modal').hide();
 
+//animation if not mobile
+if($(window).width() > 450) {
+  $('.left').show('slide', {direction: 'right'}, 500, function() {
+    if(msieversion() == 'ie')
+      $('.left').find('line').removeClass('line');
+    $('.line').addClass('move_line');
 
-$('.left').show('slide', {direction: 'right'}, 500, function() {
-  if(msieversion() == 'ie')
-    $('.left').find('line').removeClass('line');
-  $('.line').addClass('move_line');
-
-  $('.right').show('slide', {direction: 'left'}, 500, () => {
-    $('.tabs').fadeIn('slow');
+    $('.right').show('slide', {direction: 'left'}, 500, () => {
+      $('.tabs').fadeIn('slow');
+    });
+    $('.button').fadeIn('slow');
+    // $('.button').show('slide', {direction: 'left'}, 500);
   });
-  $('.button').fadeIn('slow');
-  // $('.button').show('slide', {direction: 'left'}, 500);
+} else {
+  $('.right, .button').show();
+  $('.right').addClass('center');
+}
 
-
-});
-//
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    for(var i=0; i<months.length; i++)
+      $('#month').append($("<option></option>").attr("value",months[i]).text(months[i]));
 
     //select default 'radio button'
     $('.gender').find('div').addClass('default');
