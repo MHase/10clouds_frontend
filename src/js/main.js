0eba2a11f005067326e7a96eb92d593acee6acf6
Maciej <maciek.sykula@gmail.com>
2017-05-17 02:20:44 +0200

    added: animations, tabs | changed: font, left panel (full of svg)

--- src/js/main.js
+++ src/js/main.js
@@ -1,29 +1,86 @@
-// require('../css/style.scss');
-// import style from '../css/main.scss';
-
-
 $(document).ready(() => {
 
-  //select default 'radio button'
-  $('#female.radio, #female.radio *, #male.radio, #male.radio *').addClass('default');
-  $('#male.radio, #male.radio *').addClass('selected').find('.radio_active').css({'visibility': 'visible'});
-  //
-  //
-  //
-  //select radio, deactivate another
-  $('.radio').click(function(){
-    $('.radio, .radio *').removeClass('selected');
-    $('.radio, .radio *').find('.radio_active').css({'visibility': 'hidden'});
+$('.left, .right, .tabs, .button, .modal').hide();
 
-    $(this).addClass('selected');
-    $(this).find('div').addClass('selected');
-    $(this).find('.radio_active').css({'visibility': 'visible'});
-    var val = $(this).attr('data-value');
-  });
 
-  $('.submit').click(() => {
-    // $(form).submit();
-    $('#test').click();
-    // console.log('submitted');
+$('.left').show('slide', {direction: 'right'}, 500, function() {
+  if(msieversion() == 'ie')
+    $('.left').find('line').removeClass('line');
+  $('.line').addClass('move_line');
+
+  $('.right').show('slide', {direction: 'left'}, 500, () => {
+    $('.tabs').fadeIn('slow');
   });
+  $('.button').fadeIn('slow');
+  // $('.button').show('slide', {direction: 'left'}, 500);
+
+
 });
+//
+
+    //select default 'radio button'
+    $('.gender').find('div').addClass('default');
+    $('#male.radio, #male.radio *').addClass('selected').find('.radio_active').css({
+        'visibility': 'visible'
+    });
+
+    //default selected tab
+    $('.tab[data-value="2"]').addClass('tab_active');
+
+    //toggle tabs
+    $('.tab').click(function() {
+        $('.tab').removeClass('tab_active');
+        $(this).addClass('tab_active');
+
+        // var val = $(this).attr('data-value');
+    });
+
+    //select radio, deactivate another
+    $('.radio').click(function() {
+        $('.radio, .radio *').removeClass('selected');
+        $('.radio, .radio *').find('.radio_active').css({
+            'visibility': 'hidden'
+        });
+
+        $(this).addClass('selected');
+        $(this).find('div').addClass('selected');
+        $(this).find('.radio_active').css({
+            'visibility': 'visible'
+        });
+        var val = $(this).attr('data-value');
+    });
+
+    // prevent form from submitting
+    $('form').submit((e) => {
+        e.preventDefault();
+        $('.modal, .body_inactive').show();
+    });
+    // submit form with purple div
+    $('#submit_div').click(() => {
+        $('#submit_btn').click();
+    });
+
+    //close modal
+    $('.modal .button').click(() => {
+      $('.modal, .body_inactive').hide();
+    });
+
+});
+
+
+
+
+
+
+function msieversion() {
+
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf("MSIE ");
+
+    if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))  // If Internet Explorer, return version number
+    {
+        return 'ie'
+    }
+
+    return false;
+}
